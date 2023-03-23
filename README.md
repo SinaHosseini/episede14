26e372b9c688ed1730e70b5dea537dfb3eec4411
seyed sina hosseini vaez <sshosseinivaez@gmail.com>
2023-03-23 22:29:46 +0330

    update

---
+++ README.md
@@ -0,0 +1,11 @@
+# Game Space Ship
+This is a game made by **Python**
+
+## How to play :
+Move to the right : Press key D
+
+Move to the left : Press key A
+
+For shot : Press key SPACE
+
+![Game environment]()
--- main.py
+++ main.py
@@ -6,8 +6,6 @@ from bullet import Bullet
 from space_ship import Spaceship
 from hearts import Hearts
 
-strat = time.time()
-
 
 class Game(arcade.Window):
     def __init__(self):
@@ -19,8 +17,12 @@ class Game(arcade.Window):
         self.enemies = []
         self.hearts = []
         self.score = 0
-        self.explosion_sound = arcade.load_sound(
-            ":resources:sounds/explosion2.wav")
+        self.hit_sound = arcade.load_sound(
+            ":resources:sounds/hurt5.wav")
+        self.game_over_bg = arcade.load_texture(
+            "episode14\photo_2023-03-23_21-50-37.jpg")
+        self.status = "normal"
+        self.time = time.time()
 
         for i in range(3):
             heart = Hearts(i)
@@ -28,19 +30,27 @@ class Game(arcade.Window):
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_lrwh_rectangle_textured(
-            0, 0, self.width, self.height, self.background)
 
-        self.space_ship.draw()
+        if self.status == "normal":
+            arcade.draw_lrwh_rectangle_textured(
+                0, 0, self.width, self.height, self.background)
+            score_text = f"Score: {self.score}"
+            arcade.draw_text(score_text, self.width - 120,
+                             30, arcade.color.WHITE, 20)
+            self.space_ship.draw()
 
-        for enemy in self.enemies:
-            enemy.draw()
+            for enemy in self.enemies:
+                enemy.draw()
 
-        for bullet in self.space_ship.bullets:
-            bullet.draw()
+            for bullet in self.space_ship.bullets:
+                bullet.draw()
 
-        for heart in self.hearts:
-            heart.draw()
+            for heart in self.hearts:
+                heart.draw()
+
+        elif self.status == "game over":
+            arcade.draw_lrwh_rectangle_textured(
+                0, 0, self.width, self.height, self.game_over_bg)
 
         arcade.finish_render()
 
@@ -57,17 +67,33 @@ class Game(arcade.Window):
     def on_key_release(self, symbol, modifiers: int):
         self.space_ship.change_x = 0
 
-    def make_enemy(self):
-        time.sleep(3)
-        self.enemy = Enemy(self)
-        self.enemies.append(self.enemy)
-
     def on_update(self, delta_time):
         self.space_ship.move()
 
+        if time.time() > self.time + 3:
+            self.enemy = Enemy(self)
+            self.enemies.append(self.enemy)
+            self.enemy.speed += 0.1
+            self.time = time.time()
+
         for enemy in self.enemies:
+            enemy.move()
+
             if arcade.check_for_collision(self.space_ship, enemy):
-                print("Game Over ☠️")
+                self.status = "game over"
+                self.on_draw()
+                time.sleep(3)
+                exit(0)
+
+            if len(self.hearts) > 0:
+                if enemy.center_y < 0:
+                    self.enemies.remove(enemy)
+                    self.hearts.pop()
+
+            else:
+                self.status = "game over"
+                self.on_draw()
+                time.sleep(3)
                 exit(0)
 
         for bullet in self.space_ship.bullets:
@@ -76,27 +102,14 @@ class Game(arcade.Window):
         for enemy in self.enemies:
             for bullet in self.space_ship.bullets:
                 if arcade.check_for_collision(enemy, bullet):
+                    arcade.play_sound(self.hit_sound)
+                    self.score += 1
                     self.enemies.remove(enemy)
                     self.space_ship.bullets.remove(bullet)
 
-        for enemy in self.enemies:
-            if enemy.center_y < 0:
-                self.enemies.remove(enemy)
-
         for bullet in self.space_ship.bullets:
-            if bullet.center_y > self.width:
+            if bullet.center_y > self.height:
                 self.space_ship.bullets.remove(bullet)
-
-        for enemy in self.enemies:
-            enemy.move()
-            self.enemy.speed += 0.00001
-
-        if random.randint(1, 100) == 6:
-            self.enemy = Enemy(self)
-            self.enemies.append(self.enemy)
-
-        # new_enemy = Game()
-        # new_enemy.make_enemy(self)
 
 
 window = Game()
